3d80cd45d880b59a8686dbd5fe45d9199c751b3f
Anas Al Kala <aal1580@students.fhv.at>
2021-11-27 21:23:47 +0100

    adding the image component

---
+++ src/components/ImageList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageList=(props)=>{
+    const images= props.images.map(({discription,id,urls})=>{
+        return <img alt={discription} key={id} src={urls.regular} />
+    });
+
+    return <div>{images}</div>
+}
+
+export default ImageList;
